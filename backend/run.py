4ec907700352d170feef64a1849b38f2427dc6d4
xidianwlc <xidianwlc@qq.com>
2014-07-27 14:07:32 +0800

    add new file

---
+++ backend/run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Copyright (c) 2014,WangLichao
+All rights reserved.
+
+File Name: run.py
+Author: WangLichao
+Created on: 2014-07-27
+'''
+
+import tornado
+
+
+if __name__=='__main__':
+    pass
+
